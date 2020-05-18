24146b7292af927c5dfec61bb0e3460030bd90f3
Theo <theo.farnole@gmail.com>
2020-05-18 15:42:55 +0200

    Add: [component] set a non additional scene as active on awake

---
+++ Runtime/SetSceneActiveOnAwake.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TF.MultiSceneManager;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SetSceneActiveOnAwake : MonoBehaviour
+{
+    void Start()
+    {        
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            var scene = SceneManager.GetSceneAt(i);
+
+            bool isAdditional = MultiSceneManagerData.Instance.IsSceneAdditional(scene.name);
+            if (!isAdditional)
+            {
+                SceneManager.SetActiveScene(scene);
+                Debug.LogFormat("Set {0} as active scene.", scene.name);
+            }
+        }
+    }
+}
