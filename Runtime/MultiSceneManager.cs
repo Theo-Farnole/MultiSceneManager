8cc3492cd439916ce36f12d4b0d0584302e17d44
Theo <theo.farnole@gmail.com>
2020-09-17 11:31:38 +0200

    RETAKE load masterScene first, then extra scenes

--- Runtime/MultiSceneManager.cs
+++ Runtime/MultiSceneManager.cs
@@ -10,179 +10,165 @@ using UnityEngine.Assertions;
 
 namespace TF.MultiSceneManager
 {
-    public delegate void OnSceneActivation();
+	public delegate void OnSceneActivation();
 
-    /// <summary>
-    /// SceneManager that load scene w/ logics scenes.
-    /// </summary>
-    public static class MultiSceneManager
-    {
-        #region Fields
-        private readonly static string debugLogHeader = "<color=yellow>Multi Scene Manager</color> : ";
+	/// <summary>
+	/// SceneManager that load scene w/ logics scenes.
+	/// </summary>
+	public static class MultiSceneManager
+	{
+		#region Fields
+		private readonly static string debugLogHeader = "<color=yellow>Multi Scene Manager</color> : ";
 
-        private static bool _allowSceneActivation = false;
+		private static bool _allowSceneActivation = false;
 
-        private static List<AsyncOperation> _asyncLoad = new List<AsyncOperation>();
-        private static List<AsyncOperation> _asyncUnload = new List<AsyncOperation>();
-        #endregion
+		private static List<AsyncOperation> _asyncLoad = new List<AsyncOperation>();
+		private static List<AsyncOperation> _asyncUnload = new List<AsyncOperation>();
+		#endregion
 
-        #region Properties
-        public static bool AllowSceneActivation
-        {
-            get => _allowSceneActivation; set
-            {
-                _allowSceneActivation = value;
-                UpdateAsyncOperation_AllowSceneActivation();
-            }
-        }
-        #endregion
+		#region Properties
+		public static bool AllowSceneActivation
+		{
+			get => _allowSceneActivation; set
+			{
+				_allowSceneActivation = value;
+				UpdateAsyncOperation_AllowSceneActivation();
+			}
+		}
+		#endregion
 
-        #region Methods
-        #region Public Methods
-        /// <summary>
-        /// Reload current scene with logic scenes.
-        /// </summary>
-        public static void ReloadScene()
-        {
-            LoadScene(UnitySceneManager.SceneManager.GetActiveScene().name);
-        }
+		#region Methods
+		#region Public Methods
+		/// <summary>
+		/// Reload current scene with logic scenes.
+		/// </summary>
+		public static void ReloadScene()
+		{
+			LoadScene(UnitySceneManager.SceneManager.GetActiveScene().name);
+		}
 
-        /// <summary>
-        /// Reload current scene with logic scenes asynchronously.
-        /// </summary>
-        public static void ReloadSceneAsync()
-        {
-            LoadSceneAsync(UnitySceneManager.SceneManager.GetActiveScene().name);
-        }
+		/// <summary>
+		/// Reload current scene with logic scenes asynchronously.
+		/// </summary>
+		public static void ReloadSceneAsync()
+		{
+			LoadSceneAsync(UnitySceneManager.SceneManager.GetActiveScene().name);
+		}
 
 
-        public static void SetFirstAdditionalSceneAsActive()
-        {
-            var additionalScenes = MultiSceneManagerData.Instance.DefaultAdditionalScenes;
+		public static void SetFirstAdditionalSceneAsActive()
+		{
+			var additionalScenes = MultiSceneManagerData.Instance.DefaultAdditionalScenes;
 
-            Assert.IsTrue(additionalScenes.Length >= 1, debugLogHeader + "Additional scenes is empty. Can't set first additional scene as active.");
+			Assert.IsTrue(additionalScenes.Length >= 1, debugLogHeader + "Additional scenes is empty. Can't set first additional scene as active.");
 
-            string firstAdditionalScene = additionalScenes[0];
+			string firstAdditionalScene = additionalScenes[0];
 
-            for (int i = 0; i < SceneManager.sceneCount; i++)
-            {
-                Scene currentScene = SceneManager.GetSceneAt(i);
-                string currentSceneName = currentScene.name;
+			for (int i = 0; i < SceneManager.sceneCount; i++)
+			{
+				Scene currentScene = SceneManager.GetSceneAt(i);
+				string currentSceneName = currentScene.name;
 
-                if (currentSceneName == firstAdditionalScene)
-                {
-                    SceneManager.SetActiveScene(currentScene);
-                    Debug.LogFormat(debugLogHeader + "Set first additional scene '{0}' as active.", currentSceneName);
-                    return;
-                }
-            }
+				if (currentSceneName == firstAdditionalScene)
+				{
+					SceneManager.SetActiveScene(currentScene);
+					Debug.LogFormat(debugLogHeader + "Set first additional scene '{0}' as active.", currentSceneName);
+					return;
+				}
+			}
 
-            Debug.LogWarningFormat(debugLogHeader + "Could find a scene named '{0}'. No scene has been set as active.", firstAdditionalScene);
-        }
+			Debug.LogWarningFormat(debugLogHeader + "Could find a scene named '{0}'. No scene has been set as active.", firstAdditionalScene);
+		}
 
-        public static void SetMasterSceneAsActive()
-        {
-            var additionalScenes = MultiSceneManagerData.Instance.DefaultAdditionalScenes;
+		public static void SetMasterSceneAsActive()
+		{
+			var additionalScenes = MultiSceneManagerData.Instance.DefaultAdditionalScenes;
 
-            for (int i = 0; i < SceneManager.sceneCount; i++)
-            {
-                Scene currentScene = SceneManager.GetSceneAt(i);
-                string currentSceneName = currentScene.name;
+			for (int i = 0; i < SceneManager.sceneCount; i++)
+			{
+				Scene currentScene = SceneManager.GetSceneAt(i);
+				string currentSceneName = currentScene.name;
 
-                bool isCurrentSceneInAdditionalScenes = additionalScenes.Contains(currentSceneName);
+				bool isCurrentSceneInAdditionalScenes = additionalScenes.Contains(currentSceneName);
 
-                // if it's not a additional scenes, it's a master scene
-                if (!isCurrentSceneInAdditionalScenes)
-                {
-                    SceneManager.SetActiveScene(currentScene);
-                    Debug.LogFormat(debugLogHeader + "Set master scene '{0}' as active.", currentSceneName);
-                    return;
-                }
-            }
-        }
+				// if it's not a additional scenes, it's a master scene
+				if (!isCurrentSceneInAdditionalScenes)
+				{
+					SceneManager.SetActiveScene(currentScene);
+					Debug.LogFormat(debugLogHeader + "Set master scene '{0}' as active.", currentSceneName);
+					return;
+				}
+			}
+		}
 
-        /// <summary>
-        /// Load scene with logic scenes.
-        /// </summary>
-        /// <param name="masterScene">Level to load</param>
-        public static void LoadScene(string masterScene)
-        {
-            var additionalScenes = MultiSceneManagerData.Instance.GetSceneNeeds(masterScene);
+		/// <summary>
+		/// Load scene with logic scenes.
+		/// </summary>
+		/// <param name="masterScene">Level to load</param>
+		public static void LoadScene(string masterScene)
+		{
+			SceneManager.LoadScene(masterScene);
 
-            if (additionalScenes.Length > 0)
-            {
-                SceneManager.LoadScene(additionalScenes[0]);
+			string[] additionalScenes = MultiSceneManagerData.Instance.GetSceneNeeds(masterScene);
+			for (int i = 0; i < additionalScenes.Length; i++)
+			{
+				var additionalScene = additionalScenes[i];
+				SceneManager.LoadScene(additionalScene, LoadSceneMode.Additive);
+			}
+		}
 
-                for (int i = 1; i < additionalScenes.Length; i++)
-                {
-                    var additionalScene = additionalScenes[i];
-                    SceneManager.LoadScene(additionalScene, LoadSceneMode.Additive);
-                }
+		/// <summary>
+		/// Load scene with logic scenes asynchronously.
+		/// </summary>
+		/// <param name="masterScene">Level to load</param>
+		[Obsolete("This method must be updated. Please use LoadScene instead.")]
+		public static void LoadSceneAsync(string masterScene)
+		{
+			// load master scene
+			UnitySceneManager.SceneManager.LoadScene(masterScene);
 
-                // load masterScene at the end
-                // to let managers be created first
-                SceneManager.LoadScene(masterScene, LoadSceneMode.Additive);
-            }
-            else
-            {
-                // the scene has no additional
-                // just load it
-                SceneManager.LoadScene(masterScene);
-            }
-        }
+			_asyncLoad.Clear();
+			var additionalScenes = MultiSceneManagerData.Instance.GetSceneNeeds(masterScene);
 
-        /// <summary>
-        /// Load scene with logic scenes asynchronously.
-        /// </summary>
-        /// <param name="masterScene">Level to load</param>
-        [Obsolete("This method must be updated. Please use LoadScene instead.")]
-        public static void LoadSceneAsync(string masterScene)
-        {
-            // load master scene
-            UnitySceneManager.SceneManager.LoadScene(masterScene);
+			// load additional scenes
+			foreach (var additionalScene in additionalScenes)
+			{
+				AsyncOperation ao = UnitySceneManager.SceneManager.LoadSceneAsync(additionalScene, UnitySceneManager.LoadSceneMode.Additive);
+				_asyncLoad.Add(ao); // keep reference to modify "allowSceneActivation"
+			}
 
-            _asyncLoad.Clear();
-            var additionalScenes = MultiSceneManagerData.Instance.GetSceneNeeds(masterScene);
+			UpdateAsyncOperation_AllowSceneActivation();
+		}
 
-            // load additional scenes
-            foreach (var additionalScene in additionalScenes)
-            {
-                AsyncOperation ao = UnitySceneManager.SceneManager.LoadSceneAsync(additionalScene, UnitySceneManager.LoadSceneMode.Additive);
-                _asyncLoad.Add(ao); // keep reference to modify "allowSceneActivation"
-            }
+		public static bool IsSceneLoad(string sceneToCheck)
+		{
+			// browse loaded scene
+			for (int i = 0; i < UnitySceneManager.SceneManager.sceneCount; i++)
+			{
+				var loadedScene = UnitySceneManager.SceneManager.GetSceneAt(i);
 
-            UpdateAsyncOperation_AllowSceneActivation();
-        }
+				if (loadedScene.name == sceneToCheck)
+					return true;
+			}
 
-        public static bool IsSceneLoad(string sceneToCheck)
-        {
-            // browse loaded scene
-            for (int i = 0; i < UnitySceneManager.SceneManager.sceneCount; i++)
-            {
-                var loadedScene = UnitySceneManager.SceneManager.GetSceneAt(i);
+			return false;
+		}
+		#endregion
 
-                if (loadedScene.name == sceneToCheck)
-                    return true;
-            }
+		#region Private methods        
+		static void UpdateAsyncOperation_AllowSceneActivation()
+		{
+			foreach (var ao in _asyncLoad)
+			{
+				ao.allowSceneActivation = _allowSceneActivation;
+			}
 
-            return false;
-        }
-        #endregion
-
-        #region Private methods        
-        static void UpdateAsyncOperation_AllowSceneActivation()
-        {
-            foreach (var ao in _asyncLoad)
-            {
-                ao.allowSceneActivation = _allowSceneActivation;
-            }
-
-            foreach (var ao in _asyncUnload)
-            {
-                ao.allowSceneActivation = _allowSceneActivation;
-            }
-        }
-        #endregion
-        #endregion
-    }
+			foreach (var ao in _asyncUnload)
+			{
+				ao.allowSceneActivation = _allowSceneActivation;
+			}
+		}
+		#endregion
+		#endregion
+	}
 }
